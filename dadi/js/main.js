0e74c34d6f9ef22f98b072c0d476f861f4c53ebc
BeainaT <tania.df@hotmail.it>
2022-05-03 15:04:39 +0200

    added js dice code

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,7 +1,18 @@
 // DICE
 
 //Generare un numero randomico tra 1 e 6 e assegnarlo all'utente
+const userNumber = Math.floor(Math.random() * 6 + 1);
+console.log(userNumber);
 //Generare un numero randomico tra 1 e 6 e assegnarlo al computer
+const pcNumber = Math.floor(Math.random() * 6 + 1);
+console.log(pcNumber);
 // SE il numero assegnato all'utente è > del numero assegnato al computer stampare es. "hai vinto"
 // ALTRIMENTI SE il numero assegnato all'utente è < del numero assegnato al computer stampare es. "hai perso"
-// ALTRIMENTI stampare es. "pareggio" (o generare altri numeri da assegnare)
+// ALTRIMENTI stampare es. "pareggio"
+if ( userNumber > pcNumber ) {
+    console.log("hai vinto");
+} else if ( pcNumber > userNumber ) {
+    console.log("hai perso");
+} else {
+    console.log("pareggio");
+}
