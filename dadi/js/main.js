fd6f7e90ad0834793eac1e44a3b4adbb2ea26216
BeainaT <tania.df@hotmail.it>
2022-05-03 13:48:19 +0200

    first commit

---
+++ dadi/js/main.js
@@ -0,0 +1,7 @@
+// DICE
+
+//Generare un numero randomico tra 1 e 6 e assegnarlo all'utente
+//Generare un numero randomico tra 1 e 6 e assegnarlo al computer
+// SE il numero assegnato all'utente è > del numero assegnato al computer stampare es. "hai vinto"
+// ALTRIMENTI SE il numero assegnato all'utente è < del numero assegnato al computer stampare es. "hai perso"
+// ALTRIMENTI stampare es. "pareggio" (o generare altri numeri da assegnare)
---
+++ mail/js/main.js
@@ -0,0 +1,6 @@
+//MAIL
+
+//Creare una lista di mail
+//Chiedere all'utente di inserire la sua mail
+//SE la mail inserita dall'utente è presente nella lista creata l'utente può accedere - stampare es. "accesso consentito"
+//ALTRIMENTI stampare es. "accesso negato"
