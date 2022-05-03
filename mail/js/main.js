0efbd8336c58ef4ebe93e9b2126cd13a2eca6e2f
BeainaT <tania.df@hotmail.it>
2022-05-03 15:31:38 +0200

    added js mail code

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,6 +1,15 @@
 //MAIL
 
 //Creare una lista di mail
+const allowedList = ["user-name@outlook.com", "name2022@hotmail.it", "user.10@gmail.com"];
+console.log(allowedList);
 //Chiedere all'utente di inserire la sua mail
+const userEmail = prompt("Inserisci la tua e-mail");
+console.log(userEmail);
 //SE la mail inserita dall'utente è presente nella lista creata l'utente può accedere - stampare es. "accesso consentito"
 //ALTRIMENTI stampare es. "accesso negato"
+if (allowedList.includes(userEmail)) {
+    console.log("email consentita");
+} else {
+    console.log("email non riconosciuta");
+}
