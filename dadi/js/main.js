dd213924556f203520a2d2f19b5948492e277b8d
BeainaT <tania.df@hotmail.it>
2022-05-03 17:03:25 +0200

    added basis style + js event

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,18 +1,27 @@
 // DICE
-
-//Generare un numero randomico tra 1 e 6 e assegnarlo all'utente
-const userNumber = Math.floor(Math.random() * 6 + 1);
-console.log(userNumber);
-//Generare un numero randomico tra 1 e 6 e assegnarlo al computer
-const pcNumber = Math.floor(Math.random() * 6 + 1);
-console.log(pcNumber);
-// SE il numero assegnato all'utente è > del numero assegnato al computer stampare es. "hai vinto"
-// ALTRIMENTI SE il numero assegnato all'utente è < del numero assegnato al computer stampare es. "hai perso"
-// ALTRIMENTI stampare es. "pareggio"
-if ( userNumber > pcNumber ) {
-    console.log("hai vinto");
-} else if ( pcNumber > userNumber ) {
-    console.log("hai perso");
-} else {
-    console.log("pareggio");
-}
+const gameBox = document.querySelector(".container");
+const gameBtn = document.createElement("button");
+const result = document.querySelector(".result");
+gameBox.append(gameBtn);
+gameBtn.classList.add("launch");
+gameBtn.innerText = "Lancia i dadi";
+gameBtn.addEventListener("click", 
+    function() {        
+        //Generare un numero randomico tra 1 e 6 e assegnarlo all'utente
+        const userNumber = Math.floor(Math.random() * 6 + 1);
+        let userLaunch = document.querySelector(".col_user").innerHTML = `${userNumber}`;
+        //Generare un numero randomico tra 1 e 6 e assegnarlo al computer
+        const pcNumber = Math.floor(Math.random() * 6 + 1);
+        let pcLaunch = document.querySelector(".col_pc").innerHTML = `${pcNumber}`;
+        // SE il numero assegnato all'utente è > del numero assegnato al computer stampare es. "hai vinto"
+        // ALTRIMENTI SE il numero assegnato all'utente è < del numero assegnato al computer stampare es. "hai perso"
+        // ALTRIMENTI stampare es. "pareggio"
+        if ( userNumber > pcNumber ) {
+            result.innerText = "hai vinto";
+        } else if ( pcNumber > userNumber ) {
+            result.innerText = "hai perso";
+        } else {
+            result.innerText = "pareggio";
+        }
+    }
+);
