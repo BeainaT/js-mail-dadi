87ccd9ed0fac0c9ec0917d74fb35da9d218ae661
BeainaT <tania.df@hotmail.it>
2022-05-04 10:22:47 +0200

    commented previous js and refact with  cicle

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,16 +1,38 @@
 //MAIL
 let access = document.querySelector(".enjoy");
-//Creare una lista di mail
+// //Creare una lista di mail
+// const allowedList = ["user-name@outlook.com", "name2022@hotmail.it", "user.10@gmail.com"];
+// console.log(allowedList);
+// //Chiedere all'utente di inserire la sua mail
+// const userEmail = prompt("Inserisci la tua e-mail");
+// console.log(userEmail);
+// //SE la mail inserita dall'utente è presente nella lista creata l'utente può accedere - stampare es. "accesso consentito"
+// //ALTRIMENTI stampare es. "accesso negato"
+// if (allowedList.includes(userEmail)) {
+//     access.innerText = "Welcome";
+// } else {
+//     alert("email non registrata. Non è possibile procedere con la navigazione");
+//     access.innerText = `User log in error`;
+// }
+
 const allowedList = ["user-name@outlook.com", "name2022@hotmail.it", "user.10@gmail.com"];
 console.log(allowedList);
 //Chiedere all'utente di inserire la sua mail
 const userEmail = prompt("Inserisci la tua e-mail");
 console.log(userEmail);
+//Creare una variabile booleana di controllo 
+let check = true;
 //SE la mail inserita dall'utente è presente nella lista creata l'utente può accedere - stampare es. "accesso consentito"
 //ALTRIMENTI stampare es. "accesso negato"
-if (allowedList.includes(userEmail)) {
+for( let i = 0; i < allowedList.length; i++ ) {
+    if (allowedList[i] === userEmail) {
+        check = false;
+    } 
+}
+if ( check ) {
+    alert("email non è registrata. Non è possibile procedere con la navigazione");
+    access.innerText = "User log in error";
+}
+else {
     access.innerText = "Welcome";
-} else {
-    alert("email non registrata. Non è possibile procedere con la navigazione");
-    access.innerText = `User log in error`;
 }
